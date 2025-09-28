a8ebfda1bca38e48229a87db94f09aecfd2a161f
austin <austin@austins-MacBook-Air.local>
2025-09-28 18:06:47 +0530

    call service interconnect

--- cart-service/src/main/java/com/example/cart/CartController.java
+++ cart-service/src/main/java/com/example/cart/CartController.java
@@ -1,17 +1,24 @@
 package com.example.cart;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import java.util.*;
 
 @RestController
 @RequestMapping("/cart")
 public class CartController {
     private List<String> cart = new ArrayList<>();
+    private RestTemplate restTemplate = new RestTemplate();
 
     @PostMapping("/{item}")
     public String addToCart(@PathVariable String item) {
         cart.add(item);
-        return "Added " + item + " to cart";
+
+        // Call inventory-service (Docker DNS name) to update stock
+        String inventoryUrl = "http://inventory-service:8080/inventory/deduct/" + item;
+        String inventoryResponse = restTemplate.postForObject(inventoryUrl, null, String.class);
+
+        return "Added " + item + " to cart. Inventory says: " + inventoryResponse;
     }
 
     @GetMapping
